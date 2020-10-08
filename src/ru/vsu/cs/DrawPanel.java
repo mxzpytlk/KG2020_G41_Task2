1ddfab1e3fd97fcd7b9bb170e7482f207e2946b6
mxzpytlk <mxzpytlk@gmail.com>
2020-10-08 21:19:38 +0300

    Add WooLineDrawer

--- src/ru/vsu/cs/DrawPanel.java
+++ src/ru/vsu/cs/DrawPanel.java
@@ -25,7 +25,7 @@ public class DrawPanel extends JPanel implements MouseMotionListener {
     public void paint(Graphics g) {
         BufferedImage bi = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
         Graphics bi_g = bi.createGraphics();
-        LineDrawer ld = new BriesenhamLineDrawer(new GraphicsPixelDrawer(bi_g));
+        LineDrawer ld = new WooLineDrawer(new GraphicsPixelDrawer(bi_g));
         bi_g.fillRect(0,0,getWidth(),getHeight());
         bi_g.setColor(Color.BLACK);
         drawAll(ld);
--- src/ru/vsu/cs/line_drawers/WooLineDrawer.java
+++ src/ru/vsu/cs/line_drawers/WooLineDrawer.java
@@ -14,25 +14,54 @@ public class WooLineDrawer implements LineDrawer {
 
     @Override
     public void drawLine(int x1, int y1, int x2, int y2) {
-        int x = x1;
-        int y = y1;
-        int Dx = x2 - x1;
-        int Dy = y2 - y1;
-        int e = 2 * Dy - Dx;
-        float d;
-        for (int i = 1; i <= Dx; i++) {
-            d = -1F * e / (Dy + Dx) / 1.15F;
-            if (e >= 0) {
-                pd.setPixel(x, y, new Color((int) (1F / 2 - d)));
-                pd.setPixel(x, y + 1, new Color((int) (1F / 2 + d)));
-                y++;
-                e += -2 * Dx + 2 * Dy;
-            } else {
-                pd.setPixel(x, y, new Color((int) (1F / 2 + d)));
-                pd.setPixel(x, y-1, new Color((int) (1F / 2 - d)));
-                e += 2 * Dy;
-            }
-            x++;
+        if (x2 < x1) {
+            x1 += x2;
+            x2 = x1 - x2;
+            x1 -= x2;
+            y1 += y2;
+            y2 = y1 - y2;
+            y1 -= y2;
         }
+        int dx = x2 - x1;
+        int dy = y2 - y1;
+
+        if (dx == 0 || dy == 0) {
+            new BriesenhamLineDrawer(pd).drawLine(x1, y1, x2, y2);
+            return;
+        }
+
+        float gradient;
+        pd.setPixel(x1, y1, Color.BLACK);
+
+        if (dx > dy) {
+            gradient = (float) dy / dx;
+            float intery = y1 + gradient;
+            for (int x = x1; x < x2; ++x) {
+                pd.setPixel(x, (int) intery,
+                        new Color(0, 0, 0, (int) (255 - fractionalPart(intery) * 255)));
+
+                pd.setPixel(x, (int)intery + 1,
+                        new Color(0, 0, 0, (int) (fractionalPart(intery) * 255)));
+
+                intery += gradient;
+            }
+        } else {
+            gradient = (float) dx / dy;
+            float interx = x1 + gradient;
+            for (int y = y1; y < y2; ++y) {
+                pd.setPixel((int) interx, y,
+                        new Color(0, 0, 0, (int) (255 - fractionalPart(interx) * 255)));
+
+                pd.setPixel((int) interx + 1, y,
+                        new Color(0, 0, 0, (int) (fractionalPart(interx) * 255)));
+                interx += gradient;
+            }
+        }
+
+        pd.setPixel(x2, y2, Color.BLACK);
+    }
+
+    private float fractionalPart(float x) {
+        return x - (int)x;
     }
 }
