d7ea420bfc9ccc6e4c9343076a4a7e11fe08cea2
mxzpytlk <mxzpytlk@gmail.com>
2020-10-23 19:45:45 +0300

    Improve code style

--- src/ru/vsu/cs/line_drawers/WooLineDrawer.java
+++ src/ru/vsu/cs/line_drawers/WooLineDrawer.java
@@ -17,13 +17,21 @@ public class WooLineDrawer implements LineDrawer {
     public void drawLine(int x1, int y1, int x2, int y2) {
         boolean steep = Math.abs(y1 - y2) > Math.abs(x1 - x2);
         if (steep) {
-            int temp = x1; x1 = y1; y1 = temp;
-            temp = x2; x2 = y2; y2 = temp;
+            int val = x1;
+            x1 = y1;
+            y1 = val;
+            val = x2;
+            x2 = y2;
+            y2 = val;
         }
 
         if (x1 > x2) {
-            int temp = x1; x1 = x2; x2 = temp;
-            temp = y1; y1 = y2; y2 = temp;
+            int val = x1;
+            x1 = x2;
+            x2 = val;
+            val = y1;
+            y1 = y2;
+            y2 = val;
         }
 
         double dx = x2 - x1;
@@ -33,14 +41,13 @@ public class WooLineDrawer implements LineDrawer {
         double y = y1 + gradient;
         for (int x = x1 + 1; x < x2; ++x) {
             int intY = (int) y;
-            pd.setPixel(
-                    steep ? intY + 1 : x, steep ? x : intY + 1,
-                    new Color(0, 0, 0, (float)  (y - intY))
-            );
-            pd.setPixel(
-                    steep ? intY : x, steep ? x : intY,
-                    new Color(0, 0, 0, (float)  (1 - (y - intY)))
-            );
+            if (steep) {
+                pd.setPixel(intY + 1, x, new Color(0, 0, 0, (float)  (y - intY)));
+                pd.setPixel(intY,  x, new Color(0, 0, 0, (float)  (1 - (y - intY))));
+            } else {
+                pd.setPixel(x, intY + 1, new Color(0, 0, 0, (float) (y - intY)));
+                pd.setPixel(x, intY, new Color(0, 0, 0, (float)  (1 - (y - intY))));
+            }
             y += gradient;
         }
     }
